d1921b96495c846ec186bfe94513239f178962c8
jrpbjr <jrpbjr@gmail.com>
2024-06-10 16:58:22 -0300

    adicionando o DTO do product

--- src/main/java/com/jrpbjr/catproduct/domain/product/ProductDTO.java
+++ src/main/java/com/jrpbjr/catproduct/domain/product/ProductDTO.java
@@ -1,2 +1,5 @@
-package com.jrpbjr.catproduct.domain.product;public record ProductDTO() {
+package com.jrpbjr.catproduct.domain.product;
+
+public record ProductDTO(String title, String description,String ownerId, Integer price, String categoryId) {
+
 }
