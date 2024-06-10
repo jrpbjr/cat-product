d3200f9c3ee1b37f39718d4a5ae8570eda3d2c15
jrpbjr <jrpbjr@gmail.com>
2024-06-10 12:52:09 -0300

    adicionando o domain Product

--- src/main/java/com/jrpbjr/catproduct/domain/product/Product.java
+++ src/main/java/com/jrpbjr/catproduct/domain/product/Product.java
@@ -1,2 +1,21 @@
-package com.jrpbjr.catproduct.domain.product;public class Product {
+package com.jrpbjr.catproduct.domain.product;
+
+import com.jrpbjr.catproduct.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private Integer price;
+    private Category category;
 }
