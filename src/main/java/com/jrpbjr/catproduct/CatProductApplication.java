6b91dd6616af9cbee62cb2b1359d3337be6c2d59
jrpbjr <jrpbjr@gmail.com>
2024-06-10 09:42:56 -0300

    first commit

---
+++ src/main/java/com/jrpbjr/catproduct/CatProductApplication.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.catproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jrpbjr/catproduct/CatProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.catproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
