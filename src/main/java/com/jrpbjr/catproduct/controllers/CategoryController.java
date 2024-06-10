61a10ba4218c9df5f5b04a4ad23144aefeb8f388
jrpbjr <jrpbjr@gmail.com>
2024-06-10 15:43:26 -0300

    adicionando controller e o servico para o repositorio para categoria

--- src/main/java/com/jrpbjr/catproduct/controllers/CategoryController.java
+++ src/main/java/com/jrpbjr/catproduct/controllers/CategoryController.java
@@ -1,2 +1,42 @@
-package com.jrpbjr.catproduct.controllers;public class CategoryController {
+package com.jrpbjr.catproduct.controllers;
+
+import com.jrpbjr.catproduct.domain.category.Category;
+import com.jrpbjr.catproduct.domain.category.CategoryDTO;
+import com.jrpbjr.catproduct.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/category")
+public class CategoryController {
+    private final CategoryService service;
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryData){
+        Category newCategory = this.service.insert(categoryData);
+        return ResponseEntity.ok().body(newCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll(){
+        List<Category> categories = this.service.getAll();
+        return ResponseEntity.ok().body(categories);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryData){
+        Category updatedCategory = this.service.update(id, categoryData);
+        return ResponseEntity.ok().body(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> delete(@PathVariable("id") String id){
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/jrpbjr/catproduct/services/CategoryService.java
+++ src/main/java/com/jrpbjr/catproduct/services/CategoryService.java
@@ -1,2 +1,52 @@
-package com.jrpbjr.catproduct.services;public class CategoryService {
+package com.jrpbjr.catproduct.services;
+
+import com.jrpbjr.catproduct.domain.category.Category;
+import com.jrpbjr.catproduct.domain.category.CategoryDTO;
+import com.jrpbjr.catproduct.domain.category.exceptions.CategoryNotFoundException;
+import com.jrpbjr.catproduct.repositories.CategoryRespository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRespository repository;
+
+
+    public Category insert(CategoryDTO categoryData){
+        Category newCategory = new Category(categoryData);
+
+        this.repository.save(newCategory);
+
+        return newCategory;
+    }
+
+    public Category update(String id, CategoryDTO categoryData){
+        Category category = this.repository.findById(id)
+                .orElseThrow(CategoryNotFoundException::new);
+
+        if(!categoryData.title().isEmpty()) category.setTitle(categoryData.title());
+        if(!categoryData.description().isEmpty()) category.setDescription(categoryData.description());
+
+
+        this.repository.save(category);
+
+        return category;
+    }
+
+    public void delete(String id){
+        Category category = this.repository.findById(id)
+                .orElseThrow(CategoryNotFoundException::new);
+
+        this.repository.delete(category);
+    }
+
+    public List<Category> getAll(){
+        return this.repository.findAll();
+    }
+
 }
